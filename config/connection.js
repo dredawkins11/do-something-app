95c0a8a771cf65342fb790efe1b5f53bb0afc34f
Dre Dawkins <63630450+dredawkins11@users.noreply.github.com>
2023-02-23 16:07:57 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-  console.log(sequelize);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
